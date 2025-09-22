14516ac4d75e8e8ad8e7a546cd69d6d285b97d50
Balázs Szabó <balazs.szabo.mw@gmail.com>
2025-09-22 22:00:53 +0200

    added basic login functionality

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import AuthProvider from './auth/AuthContext.jsx'
+import Login from './auth/login.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <Login/>
+      <App />
+    </AuthProvider>
   </StrictMode>,
 )
