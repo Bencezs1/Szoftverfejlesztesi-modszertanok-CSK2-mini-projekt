dd8f60150185b2dec81184cf2de1c1e5fb7441e2
Balázs Szabó <balazs.szabo.mw@gmail.com>
2025-09-23 21:03:18 +0200

    routing to main page after logging in

--- frontend/src/auth/login.jsx
+++ frontend/src/auth/login.jsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
 import { useAuth } from "./useAuth";
+import { useNavigate } from "react-router";
 
 export default function Login() {
   const { login } = useAuth();
+  const navigate = useNavigate();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
-  const handleLogin = async (e) => {
+ const handleSubmit = async (e) => {
     e.preventDefault();
+    setError(null);
+
     try {
       const res = await fetch("http://127.0.0.1:8000/api/token/", {
         method: "POST",
@@ -16,33 +20,37 @@ export default function Login() {
         body: JSON.stringify({ username, password }),
       });
 
-      if (!res.ok) throw new Error("Login failed");
+      if (!res.ok) {
+        throw new Error("Invalid username or password");
+      }
 
       const data = await res.json();
-      login(data); // store tokens in context + localStorage
-      setError("");
-      alert("Logged in!");
+      login(data);
+      navigate("/");
     } catch (err) {
       setError(err.message);
     }
   };
 
   return (
-    <form onSubmit={handleLogin}>
-      <input
-        type="text"
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button type="submit">Login</button>
-      {error && <p>{error}</p>}
-    </form>
+    <div className="login-page">
+      <h2>Bejelentkezés</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Felhasználónév"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Jelszó"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Belépés</button>
+      </form>
+      {error && <p style={{ color: "red" }}>{error}</p>}
+    </div>
   );
-}
+};
