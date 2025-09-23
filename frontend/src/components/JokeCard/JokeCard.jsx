f2583d74ecc13390d03e33f31c28ff2f0fcf9f68
Bencezs1 <zsbence2003@gmail.com>
2025-09-23 10:50:25 +0200

    add: new joke page with modal

--- frontend/src/components/JokeCard/JokeCard.jsx
+++ frontend/src/components/JokeCard/JokeCard.jsx
@@ -3,12 +3,6 @@ import Rating from '@mui/material/Rating';
 
 const JokeCard = () => {
 
-    const cardStyle = {
-        width: "30rem",
-        padding: "10px",
-        margin: "10px"
-    }
-
     const cardHeadStyle = {
         marginBottom: "50px",
         marginTop: "20px"
@@ -18,13 +12,23 @@ const JokeCard = () => {
         marginBottom: "40px"
     }
 
+    const deletStyle = {
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center"
+    }
+
     const [rating, setRating] = useState(3);
+    const [current, setCurrent] = useState(3);
 
 
   return (
-    <div className="card" style={cardStyle}>
+    <div className="card">
       <div className="card-body">
-        <h5 className="card-title" style={cardHeadStyle}>Card title</h5>
+        <div style={deletStyle}>
+          <h5 className="card-title" style={cardHeadStyle}>Card title</h5>
+          <button type="button" className="btn btn-danger">Törlés</button>
+        </div>
         <p className="card-text" style={cardBodyStyle}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque porttitor mi sit amet varius congue. Curabitur gravida, nunc id ornare placerat, libero mauris hendrerit neque, at ultricies dui felis quis enim. Vestibulum non vehicula lacus. Ut feugiat suscipit risus, ut posuere ex consectetur ut. Nam id mollis leo, at sollicitudin dui. Maecenas sit amet leo lorem. Cras lobortis eu purus id posuere. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed blandit, metus ut malesuada semper, quam erat maximus tellus, nec facilisis tortor lectus vel erat. Phasellus commodo quam eros, id rhoncus leo placerat id.
         </p>
--- frontend/src/components/Navbar/Registered.jsx
+++ frontend/src/components/Navbar/Registered.jsx
@@ -1,22 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ReactModal from 'react-modal';
 import './registered.css'
+import { justifyContent } from '@mui/system';
 
 const Registered = () => {
+  const [showForm, setShowForm] = useState(false);
+  const [joke, setJoke] = useState("");
+
+  const handleOpen = () => setShowForm(true);
+  const handleClose = () => setShowForm(false);
+
+
+
   return (
-    <nav>
-      <div className="container">
+    <>
+      <nav>
+        <div className="container">
           <div className="logo">Logo</div>
           <div className="middle">
             <button className="favourites">Kedvencek</button>
             <button className="ownJokes">Saját viccek</button>
           </div>
           <div className="right">
-            <button className="newJoke">Új vicc</button>
+            <button className="newJoke" onClick={handleOpen}>Új vicc</button>
             <button className="profile">Profil</button>
           </div>
-      </div>
-    </nav>
+        </div>
+      </nav>
 
+
+      <ReactModal isOpen={showForm}
+       onRequestClose={handleClose}
+       contentLabel='Új vicc hozzáadása'
+       style={{
+          overlay: {
+            backgroundColor: "rgba(0, 0, 0, 0.5)", // dark backdrop
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center"
+          },
+          content: {
+            position: "static", 
+            width: "500px",       
+            height: "400px",      
+            borderRadius: "10px",
+            padding: "20px",
+            inset: "unset"      
+          }}}
+       >
+          <h2>Új vicc hozzáadáas</h2>
+          <form action="post">
+            <div class="mb-3">
+              <label for="jokeName" class="form-label">Cím</label>
+              <input type="jokeName" class="form-control" id="jokeName"/>
+            </div>
+            <div class="mb-3">
+              <label for="jokeContent" class="form-label">Szöveg</label>
+              <textarea class="form-control" id="jokeContent" rows="3"></textarea>
+            </div>
+            <div className='mb-3' style={{display: "flex", justifyContent: "space-between"}}>
+              <button type="button" class="btn btn-success">Mentés</button>
+              <button type="button" onClick={handleClose} class="btn btn-danger">Mégsem</button>
+            </div>
+          </form>
+       </ReactModal>
+    </>
   )
 }
 
